2c683030320a3133eec642c15d02a139d5d0e266
nikitenko <nikitenko.max@gmail.com>
2022-07-21 08:24:36 +0300

    fix hydration failed

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import DataProvider from '../redux/store';
 import { refreshToken } from '../redux/slices/authSlice';
@@ -7,13 +7,23 @@ import UserDescriptionModal from './../components/modal/UserDescriptionModal';
 import '../styles/globals.css';
 
 const App = ({ children }) => {
+  const [showChild, setShowChild] = useState(false);
   const dispatch = useDispatch();
-
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
   useEffect(() => {
     dispatch(refreshToken());
   }, [dispatch]);
 
-  return <>{children}</>;
+  if (!showChild) {
+    return null;
+  }
+  if (typeof window === 'undefined') {
+    return <></>;
+  } else {
+    return <>{children}</>;
+  }
 };
 
 function MyApp({ Component, pageProps }) {
--- pages/api/home/index.js
+++ pages/api/home/index.js
@@ -10,38 +10,18 @@ connectDB();
 // res: An instance of http.ServerResponse, plus some helper functions
 
 const handler = async (req, res) => {
-  // To handle different HTTP methods in an API route, you can use --> req.method in your request handler
   if (req.method !== 'GET')
     return res
       .status(405)
       .json({ msg: `${req.method} method is not allowed for this endpoint.` });
 
   const latestJob = await Job.aggregate([
-    //   const jobSchema = new mongoose.Schema({
-    //     organization: {
-    //       type: mongoose.Types.ObjectId,
-    //       ref: 'organization',
-    //       required: true,
-    //     },
-    //     category: {
-    //       type: mongoose.Types.ObjectId,
-    //       ref: 'category',
-    //       required: true,
-    //     },
-    //   });
     {
       $lookup: {
         from: 'organizations',
         let: { org_id: '$organization' },
         pipeline: [
           { $match: { $expr: { $eq: ['$_id', '$$org_id'] } } },
-          //   const organizationSchema = new mongoose.Schema(
-          //   {
-          //  user: {
-          //  type: mongoose.Types.ObjectId,
-          //  ref: 'user',
-          //  required: true,
-          // },})
           {
             $lookup: {
               from: 'users',
@@ -62,13 +42,6 @@ const handler = async (req, res) => {
 
   const categoryDisplay = await Job.aggregate([
     {
-      //   const jobSchema = new mongoose.Schema({
-      //     category: {
-      //       type: mongoose.Types.ObjectId,
-      //       ref: 'category',
-      //       required: true,
-      //     },
-      //   });
       $lookup: {
         from: 'categories',
         localField: 'category',
